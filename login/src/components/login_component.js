8f15b504a1481e6bd21c49d4c90bbed857958e02
DevRoute66 <company.route66@gmail.com>
2024-05-11 20:21:19 -0300

    Update login_component.js

--- login/src/components/login_component.js
+++ login/src/components/login_component.js
@@ -14,7 +14,7 @@ export default class Login extends Component {
     e.preventDefault();
     const { email, password } = this.state;
     console.log(email, password);
-    fetch("http://localhost:5000/login-user", {
+    fetch("route66alpha.vercel.app", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
