84dce4efc3d14ecbab98a8e2773c8fa76d8fcb26
DevRoute66 <company.route66@gmail.com>
2024-05-11 20:59:54 -0300

    Update vercel.js

--- server/vercel.js
+++ server/vercel.js
@@ -2,7 +2,7 @@
   "version": 2,
   "routes": [
     {
-      "src": "/login-user",
+      "src": "/register",
       "methods": ["POST"],
       "headers": {
         "Access-Control-Allow-Origin": "https://route66alpha-ym2h.vercel.app"
